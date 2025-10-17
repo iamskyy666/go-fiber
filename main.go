eb1350acc75502b5532783d086c52e35216e27e2
Soumadip <39thdecember@gmail.com>
2025-10-18 03:40:52 +0530

    read and clear cookies 🍪

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 )
 
 func main() {
-	// set cookies
+	// get cookies 🍪
 	app := fiber.New()
-	app.Get("/", func (ctx *fiber.Ctx)error  {
+	app.Get("/login", func (ctx *fiber.Ctx)error  {
 		// create cookie
 		cookieAuth := new(fiber.Cookie)
-		cookieAuth.Name = "password"
-		cookieAuth.Value = "123456"
+		cookieAuth.Name = "username"
+		cookieAuth.Value = "john doe"
 		cookieAuth.Expires = time.Now().Add(24 * time.Hour)
 
 		cookieTheme:= new(fiber.Cookie)
@@ -30,9 +30,23 @@ func main() {
 		
 	})
 
+	// Read Cookies 
+	app.Get("/checkout", func (ctx *fiber.Ctx)error  {
+		fmt.Println("username:",ctx.Cookies("username","no user found!"))
+		fmt.Println("app_theme:",ctx.Cookies("app_theme","light"))
+		return ctx.SendStatus(fiber.StatusOK)
+		
+	})
+
+	// Clear Cookies
+	app.Get("/logout", func (ctx *fiber.Ctx)error  {
+		ctx.ClearCookie() // clear all cookies
+		// ctx.ClearCookie("app_theme") // clear specific cookies
+			return ctx.SendStatus(fiber.StatusOK)
+	})
+
 
 	fmt.Println("Server Up N Running.. ✅")
 	log.Fatal(app.Listen(":3000"))
 }
 
-// CookieParser & ClearCookie
