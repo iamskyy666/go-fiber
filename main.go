3fd711fb81a9df0bf6e2c38164467e65de9c65ba
Soumadip <39thdecember@gmail.com>
2025-10-18 03:14:49 +0530

    bodyParser 🔥

--- main.go
+++ main.go
@@ -7,15 +7,38 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Pokemon struct{
+	Name string `json:"name"`
+	Trainer string `json:"trainer"`
+}
+
 func main() {
-	// download
+	// bodyParser
 
 	app := fiber.New()
-	app.Get("/download", func (ctx *fiber.Ctx)error  {
-		return ctx.Download("./files/sample.txt")
+	app.Post("/api/pokemon", func (ctx *fiber.Ctx)error  {
+		p:=new(Pokemon)
+
+		if err:=ctx.BodyParser(p); err!=nil{
+			return err
+		}
+
+		// log.Println(p.Name)
+		// log.Println(p.Trainer)
+
+		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+			"name":p.Name,
+			"trainer":p.Trainer,
+		})
 	})
 
 
 	fmt.Println("Server Up N Running.. ✅")
 	log.Fatal(app.Listen(":3000"))
 }
+
+// JSON-Body:
+// {
+//   "name":"Blaziken 🔥",
+//   "trainer":"May"
+// }
