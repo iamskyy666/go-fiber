66cafcadd5e61d8b49ba41d4f1baf84994eee9b9
Soumadip <39thdecember@gmail.com>
2025-10-18 22:29:55 +0530

    simple-send ☑️

--- main.go
+++ main.go
@@ -9,24 +9,16 @@ import (
 
 
 func main() {
+	// Send, SendStatus, SendFile & Status
+	// Send sets the HTTP response body without copying it. From this point onward the body argument must not be changed.
 	app := fiber.New()
 	
+	app.Get("/:name",func (ctx *fiber.Ctx) error {
+		name:= []byte(ctx.Params("name"))
 
-type keyType struct{}
-var userKey keyType
+		return ctx.Send(name)
+	})
 
-app.Use(func(c *fiber.Ctx) error {
-  c.Locals(userKey, "admin") // Stores the string "admin" under a non-exported type key
-  return c.Next()
-})
-
-app.Get("/admin", func(c *fiber.Ctx) error {
-  user, ok := c.Locals(userKey).(string) // Retrieves the data stored under the key and performs a type assertion
-  if ok && user == "admin" {
-    return c.Status(fiber.StatusOK).SendString("Welcome, admin!")
-  }
-  return c.SendStatus(fiber.StatusForbidden)
-})
 	fmt.Println("Server Up N Running.. ✅")
 	log.Fatal(app.Listen(":3000"))
 
