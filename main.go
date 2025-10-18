0e731fcf33ae64e55e66b832176461cfdcb5d361
Soumadip <39thdecember@gmail.com>
2025-10-18 22:03:55 +0530

    file-uploading 📂

--- main.go
+++ main.go
@@ -7,40 +7,23 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type QueryProduct struct{
-	Sort string `query:"sort"`
-	Date int `query:"date"`
-	New bool `query:"new"`
-	Maxprice float64 `query:"maxprice"`
-}
-
 
 func main() {
-	// 💡 QueryParser : Binds the query string to a struct.
 	app := fiber.New()
-	// GET http://example.com/?sort=asc&date=today&new=true&maxprice=999.99
-
-app.Get("/", func(ctx *fiber.Ctx) error {
-	queryProduct:=new(QueryProduct)
-	if err := ctx.QueryParser(queryProduct); err!=nil{
+	
+app.Post("/upload-document", func(ctx *fiber.Ctx) error {
+	// FormFile returns the first file by key from a MultipartForm.
+	// SaveFile saves any multipart file to disk.
+	
+	file,err:=ctx.FormFile("document")
+	if err!=nil{
 		return err
 	}
 
-	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
-		"sort": queryProduct.Sort,
-		"date":queryProduct.Date,
-		"new":queryProduct.New,
-		"maxprice":queryProduct.Maxprice,
-	})
+	return ctx.SaveFile(file,fmt.Sprintf("./%s",file.Filename))
+
 
 })
 	fmt.Println("Server Up N Running.. ✅")
 	log.Fatal(app.Listen(":3000"))
 }
-
-// {
-//   "date": 234567,
-//   "maxprice": 999.99,
-//   "new": true,
-//   "sort": "asc"
-// }
